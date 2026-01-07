e9391f3e2fe4d6f437e5b60417fe8b2e8bf74afd
Ahnad Fauzan <fauzan18296@gmail.com>
2026-01-07 18:20:34 +0700

    add: Change name file script python

---
+++ face_detection.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+
+# Load the image
+image = cv.imread('images/image.jpg')
+
+# Create haarcascade model for classifiers face detection image
+face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Convert the image to grayscale
+gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+# Detect the faces in image
+faces = face_cascade.detectMultiScale(gray_image, 2.3, 5)
+
+# Draw a rectangles around faces
+for (x, y, w, h) in faces:
+    cv.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+# Display the image with detected faces
+if cv.imwrite('images/result_detected.jpg', image):
+    print("Image saved successfully with detected faces.")
+cv.waitKey(0)
+cv.destroyAllWindows()
